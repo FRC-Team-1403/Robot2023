a382ca8fa176c89accbc540427c6efc27fd7b677
CougarRoboticsProg1 <cougarroboticsprog1@gmail.com>
2023-02-15 15:51:46 -0500

    Auto Test

--- src/main/java/team1403/robot/chargedup/swerve/SwerveDrivePath.java
+++ src/main/java/team1403/robot/chargedup/swerve/SwerveDrivePath.java
@@ -112,6 +112,7 @@ public class SwerveDrivePath extends CommandBase {
 
   @Override
   public void end(boolean interrupted) {
+    m_drivetrain.decreaseSpeed(0.4);
     m_drivetrain.stop();
   }
 }
