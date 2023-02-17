eacee5a60c98a32cc5f93d7a0e8aca544b7f1ca1
CougarRoboticsProg1 <cougarroboticsprog1@gmail.com>
2023-02-17 18:42:24 -0500

    Squashed commit of the following:
    commit 8a5c9eb03bbe9fe46a8bd8c99be5cfdf80fbd8c3
    Author: CougarRoboticsProg1 <cougarroboticsprog1@gmail.com>
    Date:   Fri Feb 17 18:34:57 2023 -0500
    
        Swerve now works on the robot (#37)

--- src/main/java/team1403/robot/chargedup/CougarRobotImpl.java
+++ src/main/java/team1403/robot/chargedup/CougarRobotImpl.java
@@ -8,7 +8,6 @@ import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import team1403.lib.core.CougarLibInjectedParameters;
@@ -23,10 +22,6 @@ import team1403.robot.chargedup.swerve.SwerveDrivePath;
 import team1403.robot.chargedup.RobotConfig.DriverConfig;
 import team1403.robot.chargedup.swerve.SwerveCommand;
 import team1403.robot.chargedup.swerve.SwerveDrivePath;
-import team1403.robot.chargedup.RobotConfig.OperatorConfig;
-import team1403.robot.chargedup.arm.Arm;
-import team1403.robot.chargedup.arm.ArmCommands;
-import team1403.robot.chargedup.swerve.SwerveCommand;
 import team1403.robot.chargedup.swerve.SwerveSubsystem;
 
 /**
@@ -57,95 +52,93 @@ public class CougarRobotImpl extends CougarRobot {
     m_builtins = new BuiltinSubsystem(parameters, logger);
 
 
-    // m_arm = new Arm(parameters);
-    // m_swerveSubsystem = SwerveSubsystem.getInstance(parameters);
- 
+    // m_arm = new Arm(parameters); 
     m_visionSubsystem = new PhotonVisionSubsystem(parameters);
+    // m_arm = new Arm(parameters);
+    m_swerveSubsystem = new SwerveSubsystem(parameters);
 
     configureOperatorInterface();
-    // configureDriverInterface();
-    // registerAutoCommands();
+    configureDriverInterface();
+    registerAutoCommands();
 
   }
 
   @Override
   public Command getAutonomousCommand() {
-    return m_reader.importScript("TestAuto.json");
+    return m_reader.importScript("Circle.json");
   } 
+  
+
+    /**
+     * Configures the operator commands and their bindings.
+     */
+    private void configureOperatorInterface() {
+      XboxController xboxOperator = getJoystick("Operator", OperatorConfig.pilotPort);
+
+      new Trigger(() -> xboxOperator.getYButton()).onFalse(
+          new InstantCommand(() -> switchOperatorMode()));
+    
+      if (m_armOperatorManual) {
+        manualOperatorMode(xboxOperator);
+      } else {
+        autoOperatorMode(xboxOperator);
+    }
+  }
 
   /**
-   * Configures the operator commands and their bindings.
+   * Configures the driver commands and their bindings.
    */
-  private void configureOperatorInterface() {
-    XboxController xboxOperator = getJoystick("Driver", OperatorConfig.pilotPort);
+  private void configureDriverInterface() {
+    XboxController xboxDriver = getJoystick("Driver", RobotConfig.DriverConfig.pilotPort);
 
-    new Trigger(() -> xboxOperator.getYButton()).onFalse(
-        new InstantCommand(() -> m_visionSubsystem.SwitchPipeline()));
+    // The controls are for field-oriented driving:
+    // Left stick Y axis -> forward and backwards movement
+    // Left stick X axis -> left and right movement
+    // Right stick X axis -> rotation
+    // Setting default command of swerve subsystem
+    m_swerveSubsystem.setDefaultCommand(new SwerveCommand(
+        m_swerveSubsystem,
+        () -> -deadband(xboxDriver.getLeftX(), 0.05),
+        () -> -deadband(xboxDriver.getLeftY(), 0.05),
+        () -> -deadband(xboxDriver.getRightX(), 0.05),
+        () -> xboxDriver.getYButtonReleased())
+    );
+
+    new Trigger(() -> xboxDriver.getLeftBumper()).onFalse(
+        new InstantCommand(() -> m_swerveSubsystem.decreaseSpeed(0.2)));
+
+    new Trigger(() -> xboxDriver.getBButton()).onFalse(
+      new InstantCommand(() -> m_swerveSubsystem.zeroGyroscope()));
     
-    // if (m_armOperatorManual) {
-    //   manualOperatorMode(xboxOperator);
-    // } else {
-    //   autoOperatorMode(xboxOperator);
-    // }
   }
-  
-  // /**
-  //  * Configures the driver commands and their bindings.
-  //  */
-  // private void configureDriverInterface() {
-  //   XboxController xboxDriver = getJoystick("Driver", DriverConfig.pilotPort);
-  //   new Trigger(() -> xboxDriver.getRawButton(1)).onFalse(new InstantCommand(() -> m_visionSubsystem.SwitchPipeline()));
 
-  //   // The controls are for field-oriented driving:
-  //   // Left stick Y axis -> forward and backwards movement
-  //   // Left stick X axis -> left and right movement
-  //   // Right stick X axis -> rotation
-  //   // Setting default command of swerve subsystem
-  //   m_swerveSubsystem.setDefaultCommand(new SwerveCommand(
-  //       m_swerveSubsystem,
-  //       () -> -deadband(xboxDriver.getLeftY(), 0.05),
-  //       () -> -deadband(xboxDriver.getLeftX(), 0.05),
-  //       () -> -deadband(xboxDriver.getRightX(), 0.05),
-  //       () -> xboxDriver.getYButtonReleased())
-  //   );
+  /**
+   * Use this to pass the autonomous command to the main {@link Robot} class.
+   *
+   * @return the command to run in autonomous
+   */
+  private void registerAutoCommands() {
+    m_reader = new CougarScriptReader((Pose2d startPose) -> {
+      double feetToMeters = 0.30478512648;
 
+      Translation2d flippedXandY = new Translation2d(
+          startPose.getY() * feetToMeters, startPose.getX() * feetToMeters);
 
-  //   new Trigger(() -> xboxDriver.getRightBumper()).onFalse(
-  //       new InstantCommand(() -> m_swerveSubsystem.increaseSpeed(0.2)));
+      Rotation2d theta = new Rotation2d(
+          startPose.getRotation().getDegrees());
 
-  //   new Trigger(() -> xboxDriver.getLeftBumper()).onFalse(
-  //       new InstantCommand(() -> m_swerveSubsystem.decreaseSpeed(0.2)));
-  // }
+      Pose2d transformedStartPose = new Pose2d(flippedXandY, theta);
+      m_swerveSubsystem.setPose(transformedStartPose);
+    });
 
-  // /**
-  //  * Use this to pass the autonomous command to the main {@link Robot} class.
-  //  *
-  //  * @return the command to run in autonomous
-  //  */
-  // private void registerAutoCommands() {
-  //   m_reader = new CougarScriptReader((Pose2d startPose) -> {
-  //     double feetToMeters = 0.30478512648;
-
-  //     Translation2d flippedXandY = new Translation2d(
-  //         startPose.getY() * feetToMeters, startPose.getX() * feetToMeters);
-
-  //     Rotation2d theta = new Rotation2d(
-  //         startPose.getRotation().getDegrees());
-
-  //     Pose2d transformedStartPose;
-
-  //     transformedStartPose = new Pose2d(flippedXandY, theta);
-  //     m_swerveSubsystem.setPose(transformedStartPose);
-  //   });
-
-  //   m_reader.registerCommand("SwerveDrivePath", (CougarScriptObject p) -> {
-  //     List<Translation2d> wayPoints = p.getPointList("Waypoints");
-  //     return new SwerveDrivePath(m_swerveSubsystem,
-  //         p.getDouble("StartAngle"),
-  //         p.getDouble("EndAngle"),
-  //         wayPoints);
-  //   });
-  // }
+    m_reader.registerCommand("SwerveDrivePath", (CougarScriptObject p) -> {
+      List<Translation2d> wayPoints = p.getPointList("Waypoints");
+      return new SwerveDrivePath(m_swerveSubsystem,
+          p.getDouble("StartAngle"),
+          p.getDouble("EndAngle"),
+          wayPoints);
+    });
+  }
 
   /**
    * Applies a deadband to the given value.
@@ -184,34 +177,61 @@ public class CougarRobotImpl extends CougarRobot {
     return new XboxController(port);
   }
 
-  // public void autoOperatorMode(XboxController xboxOperator) {
-  //   new Trigger(() -> xboxOperator.getAButton()).onFalse(
-  //     new InstantCommand(() -> m_arm.moveArm(0, 0, 0, 0)));
-  //   new Trigger(() -> xboxOperator.getBButton()).onFalse(
-  //       new InstantCommand(() -> m_arm.moveArm(0, 0, 0, 0)));
-  //   new Trigger(() -> xboxOperator.getRawButton(OperatorConfig.dPadDown)).onFalse(
-  //       new InstantCommand(() -> m_arm.moveArm(0, 0, 0, 0)));
-  //   new Trigger(() -> xboxOperator.getRawButton(OperatorConfig.dPadUp)).onFalse(
-  //     new InstantCommand(() -> m_arm.moveArm(0, 0, 0, 0)));
-  //   new Trigger(() -> xboxOperator.getRawButton(OperatorConfig.dPadRight)).onFalse(
-  //       new InstantCommand(() -> m_arm.moveArm(0, 0, 0, 0)));
-  // }
+  //TODO, figure out actual setpoint values
   
-  // private void manualOperatorMode(XboxController xboxOperator) {
-  //   m_arm.setDefaultCommand(new ArmCommands(m_arm,
-  //       () -> xboxOperator.getLeftY(),
-  //       () -> xboxOperator.getRightY(),
-  //       () -> xboxOperator.getRightTriggerAxis(),
-  //       () -> xboxOperator.getLeftTriggerAxis()));
+  /**
+   * This is the auto mode for operator.
+   * Has 5 setpoints, which will each set the arm
+   * in different positions
+   * A Button -> Ground
+   * B Button -> Shelf
+   * Dpad down Button -> Tuck
+   * DPad Up -> High
+   * DPad Right -> Mid
+   *
+   * @param xboxOperator defines which controller is being used
+   */
+  public void autoOperatorMode(XboxController xboxOperator) {
+    new Trigger(() -> xboxOperator.getAButton()).onFalse(
+      new InstantCommand(() -> m_arm.moveArm(0, 0, 0, 0)));
+    new Trigger(() -> xboxOperator.getBButton()).onFalse(
+        new InstantCommand(() -> m_arm.moveArm(0, 0, 0, 0)));
+    new Trigger(() -> xboxOperator.getRawButton(OperatorConfig.dPadDown)).onFalse(
+        new InstantCommand(() -> m_arm.moveArm(0, 0, 0, 0)));
+    new Trigger(() -> xboxOperator.getRawButton(OperatorConfig.dPadUp)).onFalse(
+      new InstantCommand(() -> m_arm.moveArm(0, 0, 0, 0)));
+    new Trigger(() -> xboxOperator.getRawButton(OperatorConfig.dPadRight)).onFalse(
+        new InstantCommand(() -> m_arm.moveArm(0, 0, 0, 0)));
+  }
 
-  // }
+  /**
+   * This is the manual mode for operator.
+   * Minutely control arm with joysticks
+   * 
+   * @param xboxOperator defines which controller is being used
+   */
+  private void manualOperatorMode(XboxController xboxOperator) {
+    m_arm.setDefaultCommand(new ArmCommands(m_arm,
+        () -> xboxOperator.getLeftY(),
+        () -> xboxOperator.getRightY(),
+        () -> xboxOperator.getRightTriggerAxis(),
+        () -> xboxOperator.getLeftTriggerAxis()));
+
+  }
+
+  /**
+   * Switches the operator mode.
+   */
+  public void switchOperatorMode() {
+    m_armOperatorManual = !m_armOperatorManual;
+  }
 
   private final BuiltinSubsystem m_builtins;
   private final PhotonVisionSubsystem m_visionSubsystem;
   private CougarScriptReader m_reader;
-  // private final Arm m_arm;
+  private final Arm m_arm;
   private boolean m_armOperatorManual = true;
-  // private final SwerveSubsystem m_swerveSubsystem;
+  private final SwerveSubsystem m_swerveSubsystem;
 
 }
 
--- src/main/java/team1403/robot/chargedup/swerve/SwerveSubsystem.java
+++ src/main/java/team1403/robot/chargedup/swerve/SwerveSubsystem.java
@@ -5,6 +5,7 @@ import com.revrobotics.CANSparkMax.IdleMode;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Twist2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.kinematics.SwerveModulePosition;
@@ -12,7 +13,8 @@ import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-import team1403.lib.device.GyroscopeDevice;
+import team1403.lib.core.CougarLibInjectedParameters;
+import team1403.lib.core.CougarSubsystem;
 import team1403.lib.device.wpi.NavxAhrs;
 import team1403.lib.util.CougarLogger;
 import team1403.lib.util.SwerveDriveOdometry;
@@ -28,23 +30,17 @@ import team1403.robot.chargedup.RobotConfig.SwerveConfig;
  * gyroscope.
  */
 public class SwerveSubsystem extends CougarSubsystem {
-  private final GyroscopeDevice m_navx2;
-  // private Pose2d m_odometryPose;
+  private final NavxAhrs m_navx2;
   private final SwerveModule[] m_modules;
 
   private ChassisSpeeds m_chassisSpeeds = new ChassisSpeeds();
   private final SwerveDrivePoseEstimator m_odometer;
 
-  private final PIDController m_driftCorrectionPid = new PIDController(0.33, 0, 0);
+  private final PIDController m_driftCorrectionPid = new PIDController(0.35, 0, 0);
   private double m_desiredHeading = 0;
   private double m_speedLimiter = 0.6;
 
-  private PIDController translationController = new PIDController(
-    RobotConfig.SwerveConfig.kPTranslation, 
-    RobotConfig.SwerveConfig.kITranslation, 
-    RobotConfig.SwerveConfig.kDTranslation);
-
-  private static SwerveSubsystem instance;
+  private double m_calc = 0;
 
   /**
    * Creates a new {@link SwerveSubsystem}.
@@ -60,29 +56,42 @@ public class SwerveSubsystem extends CougarSubsystem {
     super("Swerve Subsystem", parameters);
     CougarLogger logger = getLogger();
     m_modules = new SwerveModule[] {
-        new SwerveModule("Back Right Module",
-            CanBus.backRightDriveId, CanBus.backRightSteerId,
-            CanBus.backRightEncoderId, SwerveConfig.backRightEncoderOffset, logger),
-        new SwerveModule("Back Left Module",
-            CanBus.backLeftDriveId, CanBus.backLeftSteerId,
-            CanBus.backLeftEncoderId, SwerveConfig.backLeftEncoderOffset, logger),
         new SwerveModule("Front Left Module",
             CanBus.frontLeftDriveId, CanBus.frontLeftSteerId,
             CanBus.frontLeftEncoderId, SwerveConfig.frontLeftEncoderOffset, logger),
         new SwerveModule("Front Right Module",
             CanBus.frontRightDriveId, CanBus.frontRightSteerId,
-            CanBus.frontRightEncoderId, SwerveConfig.frontRightEncoderOffset, logger)
+            CanBus.frontRightEncoderId, SwerveConfig.frontRightEncoderOffset, logger),
+        new SwerveModule("Back Left Module",
+            CanBus.backLeftDriveId, CanBus.backLeftSteerId,
+            CanBus.backLeftEncoderId, SwerveConfig.backLeftEncoderOffset, logger),
+        new SwerveModule("Back Right Module",
+            CanBus.backRightDriveId, CanBus.backRightSteerId,
+            CanBus.backRightEncoderId, SwerveConfig.backRightEncoderOffset, logger),
     };
 
     m_odometer = new SwerveDrivePoseEstimator(SwerveConfig.kDriveKinematics, getGyroscopeRotation() , getModulePositions(), new Pose2d(0, 0, new Rotation2d(0)));
 
-
     m_navx2 = new NavxAhrs("Gyroscope");
+    addDevice(m_navx2.getName(), m_navx2);
+    new Thread(() -> {
+      while (m_navx2.isCalibrating()) {
+        try {
+          Thread.sleep(10);
+        } catch (InterruptedException e) {
+          e.printStackTrace();
+        }
+      }
+      zeroGyroscope();
+    }).start(); 
+    
 
     m_desiredHeading = getGyroscopeRotation().getDegrees();
+    SmartDashboard.putNumber("Desired Heading", m_desiredHeading);
 
     setRobotRampRate(0.0);
     setRobotIdleMode(IdleMode.kBrake);
+    
   }
 
 
@@ -167,10 +176,9 @@ public class SwerveSubsystem extends CougarSubsystem {
    * 'forwards' direction.
    */
   public void zeroGyroscope() {
-    tracef("zeroGyroscope %f", getGyroscopeRotation());
+    // tracef("zeroGyroscope %f", getGyroscopeRotation());
     m_navx2.reset();
     m_desiredHeading = 0;
-    resetOdometry();
   }
 
   /**
@@ -182,6 +190,11 @@ public class SwerveSubsystem extends CougarSubsystem {
     return m_odometer.getEstimatedPosition();
   }
 
+  /**
+   * Set the position of thte odometry.
+   *
+   * @param pose the new position of the odometry.
+   */
   public void setPose(Pose2d pose) {
     m_odometer.setPose(pose);
   }
@@ -223,57 +236,6 @@ public class SwerveSubsystem extends CougarSubsystem {
   }
 
   /**
-   * Sets the module speed and heading for all 4 modules.
-   *
-   * @param states an array of states for each module.
-   */
-  public void setModuleStates(SwerveModuleState[] states) {
-    // Prevent wheels from going back to 0 degrees as the default state.
-    if (states[0].speedMetersPerSecond < 0.001) {
-      for (int i = 0; i < m_modules.length; i++) {
-        tracef("ModuleState of %s. Speed: %f, Angle: %f", 
-            m_modules[i].getName(), 
-            states[i].speedMetersPerSecond, 
-            states[i].angle.getRadians());
-        
-        m_modules[i].set(0, m_modules[i].getSteerAngle());
-      }
-      return;
-    }
-
-    SwerveDriveKinematics.desaturateWheelSpeeds(states, SwerveConfig.kMaxSpeed);
-
-    for (int i = 0; i < m_modules.length; i++) {
-      tracef("ModuleState of %s. Speed: %f, Angle: %f", 
-            m_modules[i].getName(), 
-            states[i].speedMetersPerSecond, 
-            states[i].angle.getRadians());
-
-      m_modules[i].set((states[i].speedMetersPerSecond
-          / SwerveConfig.kMaxSpeed) * m_speedLimiter, states[i].angle.getRadians());
-    }
-  }
-
-  /**
-   * Adds rotational velocity to the chassis speed to compensate for 
-   * unwanted changes in gyroscope heading.
-   */
-  private void driftCorrection() {
-    double translationalVelocity = Math.abs(m_modules[0].getDriveVelocity());
-    if (Math.abs(m_navx2.getAngularVelocity()) > 0.2) {
-      m_desiredHeading = getGyroscopeRotation().getDegrees();
-    } else if (translationalVelocity > 0) {
-      double calc = m_driftCorrectionPid.calculate(getGyroscopeRotation().getDegrees(),
-          m_desiredHeading);
-      if (Math.abs(calc) >= 0.55) {
-        m_chassisSpeeds.omegaRadiansPerSecond += calc;
-      }
-      tracef("driftCorrection %f, corrected omegaRadiansPerSecond %f", 
-            calc, m_chassisSpeeds.omegaRadiansPerSecond);
-    }
-  }
-
-  /**
    * Stops the drivetrain.
    */
   public void stop() {
@@ -283,6 +245,71 @@ public class SwerveSubsystem extends CougarSubsystem {
 
   public Pose2d getOdometryValue() {
     return m_odometer.getEstimatedPosition();
+  /**
+   * Sets the module speed and heading for all 4 modules.
+   *
+   * @param states an array of states for each module.
+   */
+  public void setModuleStates(SwerveModuleState[] states) {
+    SwerveDriveKinematics.desaturateWheelSpeeds(
+        states, SwerveConfig.kMaxSpeed);
+
+    for (int i = 0; i < m_modules.length; i++) {
+      m_modules[i].set((states[i].speedMetersPerSecond 
+          / SwerveConfig.kMaxSpeed) * m_speedLimiter, 
+          states[i].angle.getRadians());
+    }
+  }
+
+  /**
+   * Adds rotational velocity to the chassis speed to compensate for 
+   * unwanted changes in gyroscope heading.
+   * 
+   * @param chassisSpeeds the given chassisspeeds
+   * @return the corrected chassisspeeds
+   */
+  private ChassisSpeeds translationalDriftCorrection(ChassisSpeeds chassisSpeeds) {
+
+    double translationalVelocity = Math.abs(m_modules[0].getDriveVelocity());
+    if (Math.abs(m_navx2.getAngularVelocity()) > 0.1) {
+      m_desiredHeading = getGyroscopeRotation().getDegrees();
+    } else if (translationalVelocity > 1) {
+      m_calc = m_driftCorrectionPid.calculate(getGyroscopeRotation().getDegrees(),
+          m_desiredHeading);
+      SmartDashboard.putNumber("Calc Drift Correction", m_calc);
+      if (Math.abs(m_calc) >= 0.55) {
+        m_chassisSpeeds.omegaRadiansPerSecond += m_calc;
+        SmartDashboard.putNumber("Omega Radians Correction", m_chassisSpeeds.omegaRadiansPerSecond);
+      }
+      tracef("driftCorrection %f, corrected omegaRadiansPerSecond %f", 
+            m_calc, m_chassisSpeeds.omegaRadiansPerSecond);
+    }
+    return chassisSpeeds;
+  }
+
+  /**
+   * Accounts for the drift caused by the first order kinematics 
+   * while doing both translational and rotational movement. 
+   * 
+   * <p> Looks forward one control loop to figure out where the robot 
+   * should be given the chassisspeed and backs out a twist command from that.
+   * 
+   * @param chassisSpeeds the given chassisspeeds
+   * @return the corrected chassisspeeds
+   */
+  private ChassisSpeeds rotationalDriftCorrection(ChassisSpeeds chassisSpeeds) {
+    //Assuming the control loop runs in 20ms
+    final double deltaTime = 0.02;
+
+    //The position of the bot one control loop in the future given the chassisspeed
+    Pose2d robotPoseVel = new Pose2d(chassisSpeeds.vxMetersPerSecond * deltaTime, 
+        chassisSpeeds.vyMetersPerSecond * deltaTime, 
+        new Rotation2d(chassisSpeeds.omegaRadiansPerSecond * deltaTime));
+        
+    Twist2d twistVel = new Pose2d(0, 0, new Rotation2d()).log(robotPoseVel);
+    return new ChassisSpeeds(
+            twistVel.dx / deltaTime, twistVel.dy / deltaTime, 
+            twistVel.dtheta / deltaTime);
   }
 
   @Override
@@ -291,9 +318,10 @@ public class SwerveSubsystem extends CougarSubsystem {
     m_odometer.updateWithTime(Timer.getFPGATimestamp(), getGyroscopeRotation(), getModulePositions());
     SmartDashboard.putString("Odometry", m_odometer.getEstimatedPosition().toString());
 
-    driftCorrection();
+    m_chassisSpeeds = translationalDriftCorrection(m_chassisSpeeds);
+    m_chassisSpeeds = rotationalDriftCorrection(m_chassisSpeeds);
 
-    SwerveModuleState[] states = SwerveConfig.kDriveKinematics
+    SwerveModuleState[] states = SwerveConfig.kFirstOrderDriveKinematics
         .toSwerveModuleStates(m_chassisSpeeds);
 
     setModuleStates(states);
