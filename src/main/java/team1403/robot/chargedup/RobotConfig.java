8a5c9eb03bbe9fe46a8bd8c99be5cfdf80fbd8c3
CougarRoboticsProg1 <cougarroboticsprog1@gmail.com>
2023-02-17 18:34:57 -0500

    Swerve now works on the robot (#37)

--- src/main/java/team1403/robot/chargedup/RobotConfig.java
+++ src/main/java/team1403/robot/chargedup/RobotConfig.java
@@ -7,14 +7,17 @@ import edu.wpi.first.math.util.Units;
 
 import team1403.lib.util.Dimension;
 
+
 /**
  * This class holds attributes for the robot configuration.
  *
- * <p>The RobotConfig is broken out into different areas,
+ * <p>
+ * The RobotConfig is broken out into different areas,
  * each of which is captured in a class for that area. Each
  * subsystem has its own independent config.
  *
- * <p>The "electrical" configs are treated separate and independent
+ * <p>
+ * The "electrical" configs are treated separate and independent
  * to make it easier to see how the robot should be wired and see
  * any conflicts since these ports specify their config together.
  */
@@ -40,50 +43,52 @@ public class RobotConfig {
     public static final int kStatusFrameGeneralPeriodMs = 250;
     public static final int kCanTimeoutMs = 250;
 
-    public static final double kPTurning = 0.5;
+    public static final double kPTurning = 1.125;
     public static final double kITurning = 0.0;
-    public static final double kDTurning = 5.0;
+    public static final double kDTurning = 0.0;
 
-    public static final double kPAutoTurning = 0.5;
+    public static final double kPAutoTurning = 3;
     public static final double kIAutoTurning = 0.0;
-    public static final double kDAutoTurning = 5.0;
+    public static final double kDAutoTurning = 0.0;
 
-    public static final double kPTranslation = 2.0;
-    public static final double kITranslation = 0.25;
-    public static final double kDTranslation = 0.2;
+    public static final double kPTranslation = 12;
+    public static final double kITranslation = 0.0;
+    public static final double kDTranslation = 0.5;
 
     public static final double kTrackWidth = Units.inchesToMeters(21);
     public static final double kWheelBase = Units.inchesToMeters(25.5);
 
-    public static final SwerveDriveKinematics kDriveKinematics = new SwerveDriveKinematics(
-        // Front left
-        new Translation2d(kTrackWidth / 2.0, kWheelBase / 2.0),
-        // Front right
-        new Translation2d(kTrackWidth / 2.0, -kWheelBase / 2.0),
-        // Back left
-        new Translation2d(-kTrackWidth / 2.0, kWheelBase / 2.0),
-        // Back right
-        new Translation2d(-kTrackWidth / 2.0, -kWheelBase / 2.0));
+    public static final SwerveDriveKinematics kFirstOrderDriveKinematics = 
+        new SwerveDriveKinematics(
+            // Front left
+            new Translation2d(kTrackWidth / 2.0, kWheelBase / 2.0),
+            // Front right
+            new Translation2d(kTrackWidth / 2.0, -kWheelBase / 2.0),
+            // Back left
+            new Translation2d(-kTrackWidth / 2.0, kWheelBase / 2.0),
+            // Back right
+            new Translation2d(-kTrackWidth / 2.0, -kWheelBase / 2.0));
 
-    public static final double frontLeftEncoderOffset = -Math.toRadians(180.263671875);
-    public static final double frontRightEncoderOffset = -Math.toRadians(267.1875);
-    public static final double backLeftEncoderOffset = -Math.toRadians(268.2421875);
-    public static final double backRightEncoderOffset = -Math.toRadians(153.544921875);
+    public static final double frontLeftEncoderOffset = -(4.667903537536006 + Math.PI);
+    public static final double frontRightEncoderOffset = -3.109379057044195;
+    public static final double backLeftEncoderOffset = -(4.663301595172349 + Math.PI);
+    public static final double backRightEncoderOffset = -(5.872078456026235 + Math.PI);
 
     public static final double kDriveReduction = (14.0 / 50.0) * (27.0 / 17.0) * (15.0 / 45.0);
     public static final double kSteerReduction = (15.0 / 32.0) * (10.0 / 60.0);
 
-    public static final double kSteerRelativeEncoderPositionConversionFactor = 2.0 * Math.PI 
-        / 2048.0 * kSteerReduction;
+    public static final double kSteerRelativeEncoderPositionConversionFactor = 2.0 * Math.PI
+        * SwerveConfig.kSteerReduction;
 
-    public static final double kSteerRelativeEncoderVelocityConversionFactor = 
-        kSteerRelativeEncoderPositionConversionFactor * 10.0;
+    public static final double kSteerRelativeEncoderVelocityConversionFactor = 2.0 * Math.PI
+        * SwerveConfig.kSteerReduction / 60.0;
 
     public static final double kWheelDiameterMeters = Units.inchesToMeters(4);
 
-    public static final double kMaxSpeed = 6.0;
+    public static final double kMaxSpeed = 14.5;
 
-    public static final double kMaxAngularSpeed = 14.301625486188971;
+    public static final double kMaxAngularSpeed = (kMaxSpeed
+        / Math.hypot(kTrackWidth / 2.0, kWheelBase / 2.0)); // 39.795095397
 
     public static final double kVoltageSaturation = 12.0;
     public static final double kCurrentLimit = 20.0;
@@ -91,8 +96,8 @@ public class RobotConfig {
     public static final double kMaxAccelerationMetersPerSecondSquared = 3;
     public static final double kMaxAngularAccelerationRadiansPerSecondSquared = Math.PI / 2;
 
-    public static final TrapezoidProfile.Constraints kThetaControllerConstraints = 
-        new TrapezoidProfile.Constraints(kMaxAngularSpeed, 
+    public static final TrapezoidProfile.Constraints kThetaControllerConstraints = new TrapezoidProfile.Constraints(
+        kMaxAngularSpeed,
         kMaxAngularAccelerationRadiansPerSecondSquared);
   }
 
@@ -103,49 +108,30 @@ public class RobotConfig {
    */
   public static class CanBus {
 
-    public static final int wheelIntakeMotor = 1;
-    
-    public static final int telescopicArmMotor = 1;
-
-    public static final int leftAngledArmMotor = 2; 
-
-    public static final int rightAngledArmMotor = 3;
-
-    public static final int wristMotor = 4;
-
-    public static final int frontArmSwitch = 1;
-
-    public static final int backArmSwitch = 2;
-
-    /**
-     * The can bus port for the rail motor if it is a TalonSRX.
-     *
-     * <p>Should be -1 if exampleRailSparkMotor was set.
-     */
-    public static final int exampleRailMotor = 10; // talon
-
-    /**
-     * The can bus port for the rail motor if it is a SparkMax.
-     *
-     * <p>Should be -1 if exampleRailMotor was set.
-     */
-    public static final int exampleRailSparkMotor = -1;
+    // Arm Ports
+    public static final int wheelIntakeMotor = 3;
+    public static final int telescopicArmMotor = 4;
+    public static final int leftAngledArmMotor = 5;
+    public static final int rightAngledArmMotor = 6;
+    public static final int wristMotor = 7;
+    public static final int frontArmSwitch = 9;
+    public static final int backArmSwitch = 10;
 
     // Swerve CanBus ids
     public static final int frontLeftDriveId = 1;
-    public static final int frontLeftSteerId = 2;
-    public static final int frontLeftEncoderId = 1;
+    public static final int frontLeftSteerId = 15;
+    public static final int frontLeftEncoderId = 3;
 
     public static final int frontRightDriveId = 8;
-    public static final int frontRightSteerId = 3;
-    public static final int frontRightEncoderId = 3;
+    public static final int frontRightSteerId = 7;
+    public static final int frontRightEncoderId = 1;
 
     public static final int backLeftDriveId = 14;
-    public static final int backLeftSteerId = 4;
+    public static final int backLeftSteerId = 9;
     public static final int backLeftEncoderId = 2;
 
     public static final int backRightDriveId = 2;
-    public static final int backRightSteerId = 1;
+    public static final int backRightSteerId = 16;
     public static final int backRightEncoderId = 4;
   }
 
@@ -212,9 +198,9 @@ public class RobotConfig {
 
     public static double kPhysicalArmMaxExtension = 60.218;
 
-    public static final int kP = 0; //constant for Proportional
-    public static final int kI = 0; //constant for Integral
-    public static final int kD = 0; //constant for Derivative
+    public static final int kP = 0; // constant for Proportional
+    public static final int kI = 0; // constant for Integral
+    public static final int kD = 0; // constant for Derivative
 
     public static final double kArmConversionFactor = 1;
     public static final double kWristConversionFactor = 2;
@@ -227,8 +213,6 @@ public class RobotConfig {
     public static final double kMinWristRotation = 0;
     public static final double kMinArmExtension = 5;
     public static final double kMaxArmExtension = 15;
-
-    
 
     public static final Dimension wristDimensions = new Dimension(0, 0, 0);
 
