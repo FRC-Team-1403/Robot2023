77e699a14611c508da1ce1d71d1949714654d9f2
MrMiu <alexanderjruan@gmail.com>
2023-02-18 11:30:59 -0500

    Grayhill encoder tests

--- src/main/java/team1403/robot/chargedup/ExperimentalSubsystem.java
+++ src/main/java/team1403/robot/chargedup/ExperimentalSubsystem.java
@@ -1,29 +1,35 @@
 package team1403.robot.chargedup;
 
 import com.revrobotics.AbsoluteEncoder;
+import com.revrobotics.SparkMaxRelativeEncoder.Type;
 
+import edu.wpi.first.wpilibj.AnalogEncoder;
 import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.DutyCycleEncoder;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import team1403.lib.core.CougarLibInjectedParameters;
 import team1403.lib.core.CougarSubsystem;
+import team1403.lib.device.wpi.CougarSparkMax;
 import team1403.lib.device.wpi.WpiAnalogDevice;
+import team1403.lib.device.wpi.WpiLimitSwitch;
 
 public class ExperimentalSubsystem extends CougarSubsystem{
-    private final DigitalInput photogate;
-    private final Encoder throughBore;
+    private final WpiLimitSwitch limitSwitch;
+    private final WpiAnalogDevice potentiometer;
+    private final DutyCycleEncoder grayhill;
     public ExperimentalSubsystem(String name, CougarLibInjectedParameters injectedParameters) {
         super(name, injectedParameters);
-        photogate = new DigitalInput(0);
-        throughBore = new Encoder(0,0);
-    }
+        grayhill = new DutyCycleEncoder(0);
+        limitSwitch = new WpiLimitSwitch("limitSwitch", 0);
+        potentiometer = new WpiAnalogDevice("potentiometer", 0);
+      }
 
     @Override
     public void periodic() {
-        SmartDashboard.putBoolean("photogateValues", photogate.get());
-
-        SmartDashboard.putNumber("encoderValue", throughBore.get());
-        SmartDashboard.putBoolean("encoderValue", throughBore.getDirection());
+        SmartDashboard.putBoolean("Limit Switch Value", limitSwitch.get());
+        SmartDashboard.putNumber("Potentiometer Value", potentiometer.getAnalogValue());
+        SmartDashboard.putNumber("Encoder Value", grayhill.get());
     }
 
 }
