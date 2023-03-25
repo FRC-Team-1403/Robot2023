a2d6a2ce96106940bdedc06a13daa49e5800db16
CougarRoboticsProg1 <cougarroboticsprog1@gmail.com>
2023-03-25 12:48:42 -0400

    Update AutoManager.java

--- src/main/java/team1403/robot/chargedup/AutoManager.java
+++ src/main/java/team1403/robot/chargedup/AutoManager.java
@@ -72,15 +72,15 @@ public class AutoManager {
         new Pose2d(-0.5, 0, Rotation2d.fromDegrees(0)),
         List.of(
             new Translation2d(-1, 0),
-            new Translation2d(-2.5, 0)
+            new Translation2d(-2.5, 0.1)
             ),
-        new Pose2d(-4.1, 0, Rotation2d.fromDegrees(179)),
+        new Pose2d(-5, 0, Rotation2d.fromDegrees(179)),
         m_trajectoryConfig);
 
       final Trajectory trajectory2 = TrajectoryGenerator.generateTrajectory(
-        new Pose2d(-4.1, 0, Rotation2d.fromDegrees(179)),
+        new Pose2d(-5, 0, Rotation2d.fromDegrees(179)),
         List.of(
-            new Translation2d(-2.5, 0),
+            new Translation2d(-2.5, 0.1),
             new Translation2d(-1, 0)
             ),
         new Pose2d(0, 0, Rotation2d.fromDegrees(0)),
@@ -136,7 +136,7 @@ public class AutoManager {
         new SequentialCommandGroup(
           new WaitCommand(0.1),
           new SetpointArmCommand(arm, () -> ArmStateGroup.getTuck(), false),
-          new WaitCommand(1),
+          new WaitCommand(0.8),
           new InstantCommand(() -> StateManager.getInstance().updateArmState(GamePiece.CUBE)),
           new SetpointArmCommand(arm, () -> StateManager.getInstance().getCurrentArmGroup().getFloorIntakeState(), true),
           new RunIntake(arm, -1),
